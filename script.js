3c0afcb9b689d98134bb004203a1427fe59c9c93
DeepakAntoJ <110848639+DeepakAntoJ@users.noreply.github.com>
2023-09-16 23:45:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+var swiper = new Swiper(".popular-content", {
+    slidesPerView:1,
+    spaceBetween: 10,
+    autoplay: {
+        delay: 5500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        280:{
+            slidesPerView:1,
+            spaceBetween: 10,
+        },
+        320:{
+            slidesPerView:1,
+            spaceBetween: 10,
+        },
+        510:{
+            slidesPerView:2,
+            spaceBetween: 10,
+        },
+        758:{
+            slidesPerView:3,
+            spaceBetween: 15,
+        },
+        900:{
+            slidesPerView:4,
+            spaceBetween: 20,
+        },
+
+    },
+    });
+
+    let playButton = document.getElementById("play-movie");
+    let video = document.getElementById("video-container");
+    let closeBtn = document.querySelector(".close-video");
+
+    let myvideo = document.getElementById("myvideo");
+
+    playButton.onclick = () => {
+        video.classList.add("show-video");
+        // document.getElementById("video-container").className += "show-video";
+    }
+
+
+     closeBtn.onclick = () => {
+       video.classList.remove("show-video");
+
+       myvideo.pause();
+    }
+  
